247969816c8050bb548db75074c80ccdd222a9cb
Hum Bruno <hsbruno1@gmail.com>
2022-03-09 23:09:40 +0000

    Added JSON parsing to text

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,17 @@
-// https://api.adviceslip.com/
+const id = document.querySelector(".advice__id");
+const description = document.querySelector(".advice__description");
 
 async function advice() {
   try {
     const responseData = await fetch("https://api.adviceslip.com/advice");
     const responseJSON = await responseData.json();
-    console.log(responseJSON.slip.id);
-    console.log(responseJSON.slip.advice);
+    //console.log(responseJSON.slip.id);
+    //console.log(responseJSON.slip.advice);
+    id.innerHTML = `Advice #${responseJSON.slip.id}`;
+    description.innerHTML = `"${responseJSON.slip.advice}"`;
   } catch (e) {
+    id.innerHTML = "Error";
+    description.innerHTML = "Failed to load advice. Please try again";
     console.log(e);
   }
 }
